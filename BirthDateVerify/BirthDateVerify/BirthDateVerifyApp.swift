7405157131530b57768103f14c8ba4bc37c22d4a
wiseguy16 <gergusa04@gmail.com>
2022-12-15 15:21:23 -0500

    setting up project

---
+++ BirthDateVerify/BirthDateVerify/BirthDateVerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthDateVerifyApp.swift
+//  BirthDateVerify
+//
+//  Created by Gregory Weiss on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BirthDateVerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirthDateVerify/BirthDateVerify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BirthDateVerify
+//
+//  Created by Gregory Weiss on 12/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
